62d6eb8cf3c7f6946bc9a7bd91fcf7c5687ce4d0
Ovikt0r <viktor.oblapenko@gmail.com>
2022-07-23 00:55:38 +0300

    Initial commit creating first attributes of the application

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,19 @@
+package entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Setter
+@Getter
+@ToString
+public class User {
+    static int count = 1;
+    int id = ++count;
+    String firstname;
+    String lastname;
+    double money;
+}
---
+++ src/main/java/exceptions/UserNameException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class UserNameException extends Exception{
+    public UserNameException(String s) {
+        super(s);
+    }
+}
